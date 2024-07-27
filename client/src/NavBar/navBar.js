65092a46cd64c5431b9ce703668bd110d39522b6
chakshu <chakshukota74@gmail.com>
2024-07-27 09:33:17 +0530

    NavBar UI and Routing Added

---
+++ client/src/NavBar/navBar.js
@@ -0,0 +1,18 @@
+import './navBar.css';
+import {Link} from 'react-router-dom'
+const NavBar=()=>{
+
+    return(
+       <div className="navbar-container">
+        <div className="logo">StockTradingPlatform</div>
+        <div className="menu-items">
+            <div className="item"><Link to='/home'>Home</Link></div>
+            <div className="item"><Link to='/stocks'>Stocks</Link></div>
+            <div className="item"><Link to='/login'>Login</Link></div>
+            <div className="item"><Link to='/register'>Register</Link></div>
+        </div>
+       </div>
+    );
+}
+
+export default NavBar;
