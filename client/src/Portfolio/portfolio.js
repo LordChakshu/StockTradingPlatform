262218a5980a74e267c6ec2fe4ec065167dc2534
chakshu <chakshukota74@gmail.com>
2024-08-22 06:10:41 +0530

    bought stock portfolio Added

--- client/src/Portfolio/portfolio.js
+++ client/src/Portfolio/portfolio.js
@@ -1,60 +1,108 @@
 import { useContext, useEffect } from "react";
 import "./portfolio.css";
 import { cryptoContext } from "../Crypto/cryptoContext";
+import { stockContext } from "../Stock-List/stockContext";
 
 const Portfolio = () => {
   const { buyCryptoList, cryptoData } = useContext(cryptoContext);
-  useEffect(()=>{
-    
-  },[buyCryptoList])
+  const { stockBuyList, stockData,topSPStocks} = useContext(stockContext);
+  useEffect(() => {}, [buyCryptoList,stockBuyList]);
   return (
-    <div className="portfolio-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Quantity</th>
-            <th>Price Bought</th>
-            <th>Estimation</th>
-          </tr>
-        </thead>
-        <tbody>
-          {buyCryptoList.map((crypto, index) => (
-            <tr
-              key={crypto.symbol}
-              style={{
-                backgroundColor: index % 2 === 0 ? "#f9f9f9" : "#e6f7ff",
-              }}
-            >
-              <td
+    <>
+      <div className="portfolio-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Quantity</th>
+              <th>Price Bought</th>
+              <th>Estimation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {buyCryptoList.map((crypto, index) => (
+              <tr
+                key={crypto.symbol}
                 style={{
-                  color: crypto.priceChangePercent < 0 ? "red" : "green",
+                  backgroundColor: index % 2 === 0 ? "#f9f9f9" : "#e6f7ff",
                 }}
               >
-                {crypto.symbol}
-              </td>
-              <td>{crypto.quantity}</td>
-              <td>{crypto.price} BTC</td>
-              <td>
-                {(() => {
-                  const matchingCrypto = cryptoData.find(
-                    (data) => data.symbol === crypto.symbol
-                  );
-                  const currentPrice = parseFloat(
-                    matchingCrypto?.lastPrice || 0
-                  );
-                  const purchasePrice = parseFloat(crypto.price || 0);
-                  const quantityBought = parseFloat(crypto.quantity || 0);
+                <td
+                  style={{
+                    color: crypto.priceChangePercent < 0 ? "red" : "green",
+                  }}
+                >
+                  {crypto.symbol}
+                </td>
+                <td>{crypto.quantity}</td>
+                <td>{crypto.price} BTC</td>
+                <td>
+                  {(() => {
+                    const matchingCrypto = cryptoData.find(
+                      (data) => data.symbol === crypto.symbol
+                    );
+                    const currentPrice = parseFloat(
+                      matchingCrypto?.lastPrice || 0
+                    );
+                    const purchasePrice = parseFloat(crypto.price || 0);
+                    const quantityBought = parseFloat(crypto.quantity || 0);
 
-                  return (currentPrice - purchasePrice) * quantityBought;
-                })()}{" "}
-                BTC
-              </td>
+                    return (currentPrice - purchasePrice) * quantityBought;
+                  })()}{" "}
+                  BTC
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="portfolio-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Quantity</th>
+              <th>Price Bought</th>
+              <th>Estimation</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+      {stockBuyList.map((stock, index) => {
+         let matchingStock = topSPStocks.find(
+          (data) => data.symbol === stock.symbol
+        );
+
+        if (!matchingStock) {
+          matchingStock = stockData.find(
+            (data) => data.symbol === stock.symbol
+          );
+        }
+
+        const currentPrice = matchingStock ? parseFloat(matchingStock.c) : 0;
+        const purchasePrice = parseFloat(stock.price || 0);
+        const quantityBought = parseFloat(stock.quantity || 0);
+        const estimation = (currentPrice - purchasePrice) * quantityBought;
+
+        return (
+          <tr
+            key={stock.symbol}
+            style={{
+              backgroundColor: index % 2 === 0 ? "#f9f9f9" : "#e6f7ff",
+            }}
+          >
+            <td style={{ color: stock.dp < 0 ? "red" : "green" }}>
+              {stock.symbol}
+            </td>
+            <td>{stock.quantity}</td>
+            <td>{stock.price}$</td>
+            <td>{estimation}$</td>
+          </tr>
+        );
+      })}
+    </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
